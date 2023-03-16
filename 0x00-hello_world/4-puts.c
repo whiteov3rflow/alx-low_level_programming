d3ce059ac3921e9f669841775ef8985c25016301
whiteov3rflow <djibrilow@gmail.com>
2023-03-16 13:08:46 -0700

    Puts different than printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+*/
+
+
 int main()
 {
-	puts("\"Programming is like building a multilingual puzzle	
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);	
 
 }

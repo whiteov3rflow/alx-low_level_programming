87adcbdfe8bea36be530ba826c0d99d7cdc0df8f
whiteov3rflow <djibrilow@gmail.com>
2023-03-16 14:17:16 -0700

    printf is more beautiful

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- * main: Entry point
- * Description: This program's prinf a message on the screen
- * return: Always 0: (Success)
- *
- *
+ * main - Entry point
+ * Description: 'C program for print'
+ * Return: Always 0 (Success)
  */
-
-	int main() 
-	{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		
-		return (0);
-	}
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

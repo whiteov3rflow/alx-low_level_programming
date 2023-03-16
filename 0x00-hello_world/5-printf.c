004c9b5117d91d50ba279460bf0ef62880a886bb
whiteov3rflow <djibrilow@gmail.com>
2023-03-16 14:05:48 -0700

    Printf is more beautiful

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/* main: Entry point
+ *
+ * return: Always 0: Success
+ *
+ * printf: Print something on the screen
+ *
+ */
+
+	int main() 
+	{
+		printf("with proper grammar, but the outcome is a piece of art,"\n);
+		
+		return 0;
+	}

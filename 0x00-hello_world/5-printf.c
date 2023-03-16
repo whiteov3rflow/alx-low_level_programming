596aee22f945ebf0092637f49b7b8c5396eebc3a
whiteov3rflow <djibrilow@gmail.com>
2023-03-16 14:13:48 -0700

    printf is more beautiful

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-/* main: Entry point
- *
+/**
+ *main: Entry point
+ *Description: This program's prinf a message on the screen
  * return: Always 0: Success
  *
  * printf: Print something on the screen

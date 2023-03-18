b64c11914909ba077f1cc19351798505c4537772
whiteov3rflow <djibrilow@gmail.com>
2023-03-17 18:41:34 -0700

    Print a random number each time

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
  * main - Entry point
- * Description 'This Program give a random number each time it executed '
- * return: Always 0 (Success)
+ * Description: 'This program will assign a random number to the variable n'
+ * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+			int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	
-		printf("%d is positive\n", n);
-	
-	else if (n < 0)
-	
-		printf("%d is negative\n", n);
-	else
 
-		printf("%d is zero\n", n); 
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }

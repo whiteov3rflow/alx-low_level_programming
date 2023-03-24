c4d32216ed3817ccdbcaa029d92039b2f8de4446
whiteov3rflow <djibrilow@gmail.com>
2023-03-23 19:15:19 -0700

    every number from 0 to 14 expect 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/**
- * print_most_numbers: A function that print numbers from 0 to 9 expect 2 and 4
- * followed by a new line
- */
 
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
+ */
 void print_most_numbers(void)
 {
 	int i;
-	for(i=0; i < 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i, '0');
+			_putchar(i + '0');
 		}
 	}
-	_putchar('\n');
 
+	_putchar('\n');
 }

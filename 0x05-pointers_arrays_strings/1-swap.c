f4ba9c84d14179f2bb4afc023ed1becf3a007357
whiteov3rflow <djibrilow@gmail.com>
2023-03-27 18:35:12 -0700

    Pointers values swaped

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 /**
  * swap_int - swaps values of two integers
  * @a: pointer to int a
- * @b: pointer to int b 
- * Return: void 
+ * @b: pointer to int b
+ *Return: void
  */
 
 void swap_int(int *a, int *b)

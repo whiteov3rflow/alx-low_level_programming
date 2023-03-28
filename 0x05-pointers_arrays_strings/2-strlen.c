eb9549a816f22d797ced5504da372c0d485b2120
whiteov3rflow <djibrilow@gmail.com>
2023-03-27 18:43:20 -0700

    Returning the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int_strlen - return lenght of a given string
+ * @s: String to be checked
+ * Return: void 
+ */
+
+int_strlen(char *s)
+{
+	int c = *s
+	return strlen(c);	
+}

542acf4936921d14bd42ad3391d2cf10f7010661
whiteov3rflow <djibrilow@gmail.com>
2023-03-27 19:49:47 -0700

    Printing plus newline

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts: Function that print a string
- * @str: Variable to be printed
- * _putchar: prints to stdout
+ * _puts - prints a string followed by a new line to the stdout
+ * @str: pointer to string to output
+ * _putchar prints to stdout
  */
 void _puts(char *str)
 {
@@ -10,9 +10,9 @@ void _puts(char *str)
 
 	i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
-	
+
 		_putchar(str[i]);
 
 		i++;

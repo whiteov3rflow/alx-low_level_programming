b800fb33ddf7d3d47ce7d638aeeb88d799f29cbb
whiteov3rflow <djibrilow@gmail.com>
2023-04-01 00:00:35 +0000

    Reverse an array elements by swaping them

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int j;
 
-	for(i=0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		j = a[i];
-		a = a[n-1-i];
-		a = [n-1-i] = j;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = j;
 	}
 }

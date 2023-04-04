53afe7fb013138e56c8027ebdf49f10788e057b2
whiteov3rflow <djibrilow@gmail.com>
2023-04-04 00:11:18 +0000

    Filling up the memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset() a function to fills the first n bytes of memory area
+ * @s: the pointer to the memory
+ * @b: constant byte
+ * @n: first byte of the memory to be filled
+ * Return: The pointer s in returned
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+    
+}

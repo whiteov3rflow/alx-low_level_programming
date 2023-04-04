d7f3298ff66ab860dbee2ec13a1fd3e6381bfc71
whiteov3rflow <djibrilow@gmail.com>
2023-04-04 23:23:26 +0000

    Printing a chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,9 +2,11 @@
 #define FILE_MAIN
 
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+
 #endif

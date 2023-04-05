0b9616b2a4fff829399e1cedbd8bcae75aa10e58
whiteov3rflow <djibrilow@gmail.com>
2023-04-05 02:21:54 +0000

    Setting a value of the pointer a to char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
-
+void set_string(char **s, char *to);
 #endif

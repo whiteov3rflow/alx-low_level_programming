1e2a95faa3b16d27ae81cd408b3010b4870b704f
whiteov3rflow <djibrilow@gmail.com>
2023-04-06 00:10:52 +0000

    Returning a string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * _puts_recursion- Function that return a string followed by a new line
- * @s: pointer of the string to be returned
- * Return: A string 
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {

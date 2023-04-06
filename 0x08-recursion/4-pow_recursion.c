311ffa72f06721dac983db9c9fadfc751b214c54
whiteov3rflow <djibrilow@gmail.com>
2023-04-06 22:00:23 +0000

    Calculate the power of x by y with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion: Funtion that return x power y recursively
+ * @x: The int to pow
+ * @y: The base
+ * Return: An int  
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < x)
+		return (-1);
+	return (x * _pow_recursion(x, (y - 1)));
+}

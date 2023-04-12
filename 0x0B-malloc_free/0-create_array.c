2b9c4f027972c514d38234b27f2e453c11a6555a
whiteov3rflow <djibrilow@gmail.com>
2023-04-12 01:36:40 +0000

    Update the task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-	if (size == NULL || size == 0)
+	if (size == 0 || str == NULL)
 		return (NULL);
 	
 	for (i = 0; i < size; i++)
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,4 @@
 #define FILE_MAIN
 
 char *create_array(unsigned int size, char c);
-
+#endif

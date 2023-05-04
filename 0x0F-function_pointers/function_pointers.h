7540c0ac38dd0925fe02f11ab7558800d743da98
0xWhite <35066771+whiteov3rflow@users.noreply.github.com>
2023-05-04 07:31:39 +0100

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FUNCTION_POINTER_H
-#define _FUNCTION_POINTER_H
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, int size, void (*action)(int));
-
-#endif
-
